b3f03e160a3a6502d63facb9c3ec48f896a367fe
KhalifahdGreat <abubakaronumoh@gmail.com>
2024-09-27 13:21:21 -0700

    first commit

---
+++ set_token_error.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * set_op_tok_error - Sets last element of op_toks to be an error code.
+ * @error_code: Integer to store as a string in op_toks.
+ */
+void set_op_tok_error(int error_code)
+{
+	int token_length = 0, i = 0;
+	char *exit_str = NULL;
+	char **new_token = NULL;
+
+	token_length = token_arr_len();
+	new_token = malloc(sizeof(char *) * (token_length + 2));
+	if (!op_toks)
+	{
+		malloc_error();
+		return;
+	}
+	while (i < token_length)
+	{
+		new_token[i] = op_toks[i];
+		i++;
+	}
+	exit_str = get_int(error_code);
+	if (!exit_str)
+	{
+		free(new_token);
+		malloc_error();
+		return;
+	}
+	new_token[i++] = exit_str;
+	new_token[i] = NULL;
+	free(op_toks);
+	op_toks = new_token;
+}
---
+++ tokenize_string.c
@@ -0,0 +1,166 @@
+#include <stdlib.h>
+
+char **strtow(char *str, char *delims);
+int is_delim(char ch, char *delims);
+int get_word_length(char *str, char *delims);
+int get_word_count(char *str, char *delims);
+char *get_next_word(char *str, char *delims);
+
+/**
+ * strtow - takes a string and seperates its words
+ *
+ * @str: string to seperate into words
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: 2D array of pointers to each word
+ */
+
+char **strtow(char *str, char *delims)
+{
+	char **words = NULL;
+	int word_count, wordLen, n, i = 0;
+
+	if (str == NULL || !*str)
+		return (NULL);
+	word_count = get_word_count(str, delims);
+
+
+	if (word_count == 0)
+		return (NULL);
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	while (i < word_count)
+	{
+		wordLen = get_word_length(str, delims);
+		if (is_delim(*str, delims))
+		{
+			str = get_next_word(str, delims);
+		}
+		words[i] = malloc((wordLen + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				i--;
+				free(words[i]);
+			}
+			free(words);
+			return (NULL);
+		}
+		n = 0;
+		while (n < wordLen)
+		{
+			words[i][n] = *(str + n);
+			n++;
+		}
+		words[i][n] = '\0'; /* set end of str */
+		str = get_next_word(str, delims);
+		i++;
+	}
+	words[i] = NULL; /* last element is null for iteration */
+	return (words);
+}
+
+/**
+ * is_delim - checks if stream has delimitor char
+ *
+ * @ch: character in stream
+ *
+ * @delims: Pointer to null terminated array of delimitors
+ *
+ * Return: 1 (success) 0 (failure)
+ */
+
+int is_delim(char ch, char *delims)
+{
+	int i = 0;
+
+	while (delims[i])
+	{
+		if (delims[i] == ch)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * get_word_length - gets the word length of cur word in str
+ *
+ * @str: string to get word length from current word
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: word length of current word
+ */
+
+int get_word_length(char *str, char *delims)
+{
+	int word_length = 0, pending = 1, i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+		{
+			word_length++;
+		}
+		if (word_length > 0 && is_delim(str[i], delims))
+			break;
+		i++;
+	}
+	return (word_length);
+}
+
+/**
+ * get_word_count - gets the word count of a string
+ *
+ * @str: string to get word count from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: the word count of the string
+ */
+
+int get_word_count(char *str, char *delims)
+{
+	int word_count = 0, pending = 1, i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+		{
+			pending = 0;
+			word_count++;
+		}
+		i++;
+	}
+	return (word_count);
+}
+
+/**
+ * get_next_word - gets the next word in a string
+ *
+ * @str: string to get next word from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: pointer to first char of next word
+ */
+
+char *get_next_word(char *str, char *delims)
+{
+	int pending = 0;
+	int index = 0;
+
+	while (*(str + index))
+	{
+		if (is_delim(str[index], delims))
+			pending = 1;
+		else if (pending)
+			break;
+		index++;
+	}
+	return (str + index);
+}
